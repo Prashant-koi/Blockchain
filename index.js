c02af4ced0aae61631706b9a33b52cb218a5fb6f
Prashant-koi <koiralaprashanta10@gmail.com>
2025-05-06 00:35:05 -0500

    Implement Chain and Wallet classes need to add proof of work

--- index.js
+++ index.js
@@ -58,3 +58,39 @@ class Block {
         return hash.digest('hex');
     }
 }
+class Chain {
+    constructor() {
+        this.chain = [new Block('', new Transaction(100, 'genesis', 'prasant'))];
+    }
+    get lastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(transaction, senderPublicKey, signature) {
+        const verifier = crypto.createVerify('SHA256');
+        verifier.update(transaction.toString());
+        const isValid = verifier.verify(senderPublicKey, signature);
+        if (isValid) {
+            const newBlock = new Block(this.lastBlock.hash, transaction);
+            this.chain.push(newBlock);
+        }
+    }
+}
+Chain.instance = new Chain();
+class Wallet {
+    constructor() {
+        const keypair = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+            publicKeyEncoding: { type: 'spki', format: 'pem' },
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+        });
+        this.privateKey = keypair.privateKey;
+        this.publicKey = keypair.publicKey;
+    }
+    sendMoney(amount, payeePublicKey) {
+        const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+        const sign = crypto.createSign('SHA256');
+        sign.update(transaction.toString()).end();
+        const signature = sign.sign(this.privateKey);
+        Chain.instance.addBlock(transaction, this.publicKey, signature);
+    }
+}
